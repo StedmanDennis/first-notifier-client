197625e7c3eeb80716734a9f51320f60b03a5b65
Stedman Dennis <stedmandennis@gmail.com>
2025-06-16 01:52:38 -0500

    made app a PWA

---
+++ src/app/manifest.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'FIRST Tech Notifier',
+    short_name: 'FIRST Tech Notifier',
+    description: 'Sends notifications about matches of the FIRST Tech Challenge',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
