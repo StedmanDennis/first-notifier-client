fd2fa1f2e00d80a533aac45057868400fda568ea
Stedman Dennis <stedmandennis@gmail.com>
2025-06-16 01:51:35 -0500

    test server sent events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,38 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [events, setEvents] = useState<String[]>([])
+  const [clientType, setClientType] = useState<String>("queuer")//queuer,team_associate,head_queuer
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    const notificationSource = new EventSource(`${process.env.NEXT_PUBLIC_api_base}/notifications`);
+    notificationSource.addEventListener("queuer_assigned", (event) => {
+      const { assignedQueuerId, teamNumber, previousQueuerId, matchOrder } = JSON.parse(event.data) as { matchOrder: number, teamNumber: string, assignedQueuerId?: number, previousQueuerId?: number }
+      const clientQueuerId = 1
+      const isClientEvent = assignedQueuerId == clientQueuerId || previousQueuerId == clientQueuerId
+      const isUnassignment = assignedQueuerId == null || assignedQueuerId == null
+      let message_part_1 = ''
+      let message_part_2 = ''
+      if (isClientEvent) {
+        message_part_1 = "You have been"
+      } else {
+        message_part_1 = `Queuer ${isUnassignment ? previousQueuerId : assignedQueuerId} has been`
+      }
+      if (isUnassignment) {
+        message_part_2 = `unassigned from team ${teamNumber} for match ${matchOrder}`
+      } else {
+        message_part_2 = `assigned to team ${teamNumber} for match ${matchOrder}`
+      }
+      setEvents([...events, `${message_part_1} ${message_part_2}`])
+    })
+    return () => notificationSource.close()
+  })
+
+  return (
+    <div>
+      {events.length == 0 ? "No events sent" : events.map((message, index) => <div key={index}>{message}</div>)}
     </div>
   );
 }
